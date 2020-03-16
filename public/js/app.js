604ab0fad907ed9abd9db86a091815bc12a9bb54
Eihab Tawfig <ihabtawfig@gmail.com>
2020-03-16 15:21:13 +0400

    remove coonsole command

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-console.log('Client side javascript file is loaded!')
 fetch('http://localhost:3000/weather?location=khartoum').then((response)=>{
     response.json().then( (data) => {
         console.log(data)
